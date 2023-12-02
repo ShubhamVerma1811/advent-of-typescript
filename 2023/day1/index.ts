b3d002f236cefcfdac11e2b4a211d1630354ca99
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2023-12-02 10:59:30 +0530

    fix: prettier

--- 2023/day1/index.ts
+++ 2023/day1/index.ts
@@ -1,10 +1,8 @@
-type SantasFavoriteCookies = "ginger-bread" | "chocolate-chip";
+type SantasFavoriteCookies = "ginger-bread" | "chocolate-chip"
 
+import { Equal, Expect } from "type-testing"
 
-import { Expect, Equal } from 'type-testing';
-
-
-type test_0_actual = SantasFavoriteCookies;
+type test_0_actual = SantasFavoriteCookies
 //   ^?
-type test_0_expected = 'ginger-bread' | 'chocolate-chip';
-type test_0 = Expect<Equal<test_0_actual, test_0_expected>>;
+type test_0_expected = "ginger-bread" | "chocolate-chip"
+type test_0 = Expect<Equal<test_0_actual, test_0_expected>>
