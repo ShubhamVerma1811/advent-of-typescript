497e4114d4022d855187ebdc2dbb1280654d43f0
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2023-12-13 07:45:47 +0530

    chore: biome

--- 2023/day3/index.ts
+++ 2023/day3/index.ts
@@ -1,41 +1,40 @@
 type GiftWrapper<T, U, R> = {
-  present: T;
-  from: U;
-  to: R;
+	present: T;
+	from: U;
+	to: R;
 };
 
-import { Expect, Equal } from 'type-testing';
+import { Equal, Expect } from "type-testing";
 
-type test_SantaToTrash_actual = GiftWrapper<'Car', 'Santa', 'Trash'>;
+type test_SantaToTrash_actual = GiftWrapper<"Car", "Santa", "Trash">;
 //   ^?
 type test_SantaToTrash_expected = {
-  present: 'Car';
-  from: 'Santa';
-  to: 'Trash';
+	present: "Car";
+	from: "Santa";
+	to: "Trash";
 };
 type test_SantaToTrash = Expect<
-  Equal<test_SantaToTrash_actual, test_SantaToTrash_expected>
+	Equal<test_SantaToTrash_actual, test_SantaToTrash_expected>
 >;
 
-type test_TrashToPrime_actual = GiftWrapper<'vscode', 'Trash', 'Prime'>;
+type test_TrashToPrime_actual = GiftWrapper<"vscode", "Trash", "Prime">;
 //   ^?
 type test_TrashToPrime_expected = {
-  present: 'vscode';
-  from: 'Trash';
-  to: 'Prime';
+	present: "vscode";
+	from: "Trash";
+	to: "Prime";
 };
 type test_TrashToPrime = Expect<
-  Equal<test_TrashToPrime_actual, test_TrashToPrime_expected>
+	Equal<test_TrashToPrime_actual, test_TrashToPrime_expected>
 >;
 
-type test_DanToEvan_actual = GiftWrapper<'javascript', 'Dan', 'Evan'>;
+type test_DanToEvan_actual = GiftWrapper<"javascript", "Dan", "Evan">;
 //   ^?
 type test_DanToEvan_expected = {
-  present: 'javascript';
-  from: 'Dan';
-  to: 'Evan';
+	present: "javascript";
+	from: "Dan";
+	to: "Evan";
 };
 type test_DanToEvan = Expect<
-  Equal<test_DanToEvan_actual, test_DanToEvan_expected>
+	Equal<test_DanToEvan_actual, test_DanToEvan_expected>
 >;
-
